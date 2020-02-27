1b2771ef0165c0873655f278bd99b553de02da03
Bruno Galvao <brunopgalvao@gmail.com>
2020-02-27 11:49:26 -0500

    debugging

--- script.js
+++ script.js
@@ -10,35 +10,38 @@ const upVoteArrows = document.querySelectorAll('.article-up-vote-arrow')
 // Built-in Native Sorting Algorithm
 const builtInSort = () => {
     const articles = document.querySelectorAll('article')
-    const result = Array.from(articles).sort((a, b) => {
+    const sortedArticles = Array.from(articles).sort((a, b) => {
         let first = parseInt(a.querySelector('.article-up-vote-count').innerText)
         let second = parseInt(b.querySelector('.article-up-vote-count').innerText)
         if(first == second) return 0
         else if(first > second) return -1
         else return 1
     })
-    result.forEach((article, index) => document.querySelector('.articles').append(article))
+    sortedArticles.forEach((article, index) => document.querySelector('.articles').append(article))
 }
 
 // Insertion Sort
 const insertionSort = () => {
     const sort = arr => {
         for (let i = 0; i < arr.length; i++) {
-          let el = parseInt(arr[i].querySelector('.article-up-vote-count').innerText)
+        let curr = parseInt(arr[i].querySelector('.article-up-vote-count').innerText)
           let j
 
-          for (j = i - 1; j >= 0 && arr[j] > el; j--) {
-            console.log('arr[j]', arr[j])
+          for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
+            let temp = arr[j + 1]
+            console.log(`moving ${j} to ${j+1}`)
             arr[j + 1] = arr[j]
+            console.log(`moving temp to ${j}`)
+            arr[j] = temp
           }
-          arr[j + 1] = el
+          arr[j + 1] = curr
         }
-        console.log('arr', arr)
         return arr
       }
-      const articles = Array.from(document.querySelectorAll('article'))
-      const sortedArticles = sort(articles)
-      console.log(sortedArticles)
+      console.log('articles:', document.querySelectorAll('article'))
+      const articlesArray = [...Array.from(document.querySelectorAll('article'))]
+      const sortedArticles = sort(articlesArray)
+      console.log('sortedArticles', sortedArticles)
       sortedArticles.forEach((article, index) => {
           document.querySelector('.articles').append(document.querySelectorAll('article')[article])
         })
@@ -46,7 +49,7 @@ const insertionSort = () => {
 
 const incrementByOne = () => {
     event.target.nextElementSibling.innerText = parseInt(event.target.nextElementSibling.innerText) + 1
-    builtInSort()
+    insertionSort()
 }
 
 upVoteArrows.forEach(upArrow => upArrow.addEventListener('click', incrementByOne))
