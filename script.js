0a563425941d9b37bb769092181bcb7585f311e7
Bruno Galvao <brunopgalvao@gmail.com>
2020-03-01 14:45:12 -0500

    replace with cards

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const upVoteArrows = document.querySelectorAll('.article-up-vote-arrow')
+const upVoteArrows = document.querySelectorAll('.card-up-vote-arrow')
 
 // Selection Sort
 // Bubble Sort
@@ -9,50 +9,50 @@ const upVoteArrows = document.querySelectorAll('.article-up-vote-arrow')
 
 // Built-in Native Sorting Algorithm
 const builtInSort = () => {
-    const articles = document.querySelectorAll('article')
-    const sortedArticles = Array.from(articles).sort((a, b) => {
-        let first = parseInt(a.querySelector('.article-up-vote-count').innerText)
-        let second = parseInt(b.querySelector('.article-up-vote-count').innerText)
+    const cards = document.querySelectorAll('.card')
+    const sortedCards = Array.from(cards).sort((a, b) => {
+        let first = parseInt(a.querySelector('.card-up-vote-count').innerText)
+        let second = parseInt(b.querySelector('.card-up-vote-count').innerText)
         if (first == second) return 0
         else if (first > second) return -1
         else return 1
     })
-    sortedArticles.forEach((article, index) => document.querySelector('.articles').append(article))
+    sortedCards.forEach((card, index) => document.querySelector('.cards').append(card))
 }
 
 // Bubble Sort - can i make this independent of the dom?
-const bubbleSort = (arr) => {
-    for (let i = (arr.length - 1); i >= 0; i--)
-        for (let j = (arr.length - i); j > 0; j--)
-            if (arr[j] < arr[j - 1])
-                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
-    console.log(arr)
-    return arr
+const bubbleSort = (cards) => {
+    for (let i = (cards.length - 1); i >= 0; i--)
+        for (let j = (cards.length - i); j > 0; j--)
+            if (cards[j] < cards[j - 1])
+                [cards[j - 1], cards[j]] = [cards[j], cards[j - 1]]
+    console.log(cards)
+    return cards
 }
 
 // Insertion Sort
 const insertionSort = () => {
-    const sortAsc = arr => {
-        const articles = document.querySelectorAll('article')
-        for (let i = 0; i < arr.length; i++) {
-            let curr = parseInt(arr[i].querySelector('.article-up-vote-count').innerText)
+    const sortAsc = cardsArr => {
+        const cards = document.querySelectorAll('.card')
+        for (let i = 0; i < cardsArr.length; i++) {
+            let curr = parseInt(cardsArr[i].querySelector('.card-up-vote-count').innerText)
             let j
 
-            for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
-                let temp = arr[j + 1]
+            for (j = i - 1; j >= 0 && cardsArr[j] > curr; j--) {
+                let temp = cardsArr[j + 1]
                 let temp1 = document.querySelectorAll('article')[j + 1].cloneNode(true)
-                articles[j + 1] = articles[j]
-                document.querySelector('.articles').append(articles[j + 1])
-                arr[j + 1] = arr[j]
-                articles[j] = temp1
-                document.querySelector('.articles').append(articles[j])
-                arr[j] = temp
+                cards[j + 1] = cards[j]
+                document.querySelector('.cards').append(cards[j + 1])
+                cardsArr[j + 1] = cardsArr[j]
+                cards[j] = temp1
+                document.querySelector('.cards').append(cards[j])
+                cardsArr[j] = temp
             }
-            arr[j + 1] = curr
-            articles[j + 1] = articles[i]
-            document.querySelector('.articles').append(articles[j + 1])
+            cardsArr[j + 1] = curr
+            cards[j + 1] = cards[i]
+            document.querySelector('.cards').append(cards[j + 1])
         }
-        return articles
+        return cards
     }
 
     const sortDesc = arr => {
